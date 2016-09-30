a8565013fcf6c3db706541dba724bdef2cfdb2bc
duddjek12 <abra132@naver.com>
2016-09-30 19:29:50 +0900

    create project

---
+++ gitProject/src/com_test/B01.java
@@ -0,0 +1,12 @@
+package com_test;
+
+import java.util.Scanner;
+
+public class B01 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello");
+	}
+
+}
